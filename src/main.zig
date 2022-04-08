d05be35a9d3d5082b52125439c3e035049e280ea
Cryptopragmatic <tristan.alberio@gmail.com>
2022-04-08 22:32:30 +0800

    zig assmt lesson

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+pub fn main() void{
+    std.debug.print("Hello,World\n",.{});
+    std.debug.print("Hello,{s}!\n", .{"World"});
+    
 }
+
+const speed: i32 = 5; //const is immutable value , signed 32bit
+var kilometer:u32 = 1000;// var is mutable value , unsigned 32bit
+//:type (i32/u32) is an indentifier. inferrable
+
+// @as performs an explicit type coercion
+const ispeed = @as(i32,5);
+var ikilometer = @as(u32,1000);
